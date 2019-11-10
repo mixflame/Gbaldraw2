ec081027a5fdd78c021766e7a4f08d9d2a63fee6
Jonathan Silverman <jsilverman2@gmail.com>
2019-11-10 16:52:16 -0700

    initial

---
+++ mainwindow.h
@@ -0,0 +1,53 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QList>
+#include <QMainWindow>
+
+class ScribbleArea;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    void createActions();
+    void createMenus();
+    bool maybeSave();
+    bool saveFile(const QByteArray &fileFormat);
+    ScribbleArea *scribbleArea;
+    QMenu *saveAsMenu;
+    QMenu *fileMenu;
+    QMenu *optionMenu;
+    QMenu *helpMenu;
+    QAction *openAct;
+    QList<QAction *> saveAsActs;
+    QAction *penColorAct;
+    QAction *penWidthAct;
+    QAction *printAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
+    QAction *exitAct;
+    QAction *clearScreenAct;
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
+
+private slots:
+    void open();
+    void save();
+    void penColor();
+    void penWidth();
+    void about();
+
+};
+#endif // MAINWINDOW_H
